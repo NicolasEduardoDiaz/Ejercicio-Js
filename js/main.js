3e7be543ee1267b3d03c14fd8ddce2ced77cfee4
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-05-28 07:27:55 -0500

    feat: :sparkles: datos nuevos de java script

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-var text = "hola, me llamo nicolas";
-var Number = 42;
-var Boolean = true;
-var notDefined; // undefined
-
-console.log(typeof text);   //"string"
-console.log(typeof Number); //"Number"
-console.log(typeof Boolean);    //"Boolean"
-console.log(typeof notDefined); // undefined
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
