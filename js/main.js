91d291f134a46f487c9b22aca7eedd2ee3e10658
NicolasEduardoDiaz <160768951+NicolasEduardoDiaz@users.noreply.github.com>
2024-05-30 06:33:57 -0500

    feat: :sparkles: Metodo de objetos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
+const miObjeto = {
+    nombre: "Ejemplo Objeto",
+    version: 1.0,
+    descripcion: "Un objeto de ejemplo con funcion",
+    mostrarInformacion: function(p1, p2){
+        console.log("Nombre " + this.nombre);
+        console.log("Version " + this.version);
+        console.log("Descripcion " + this.descripcion);
+        console.log("Parametro 1 " + p1);
+        console.log("Parametro 2 " + p2);
+    }
+};
+miObjeto.mostrarInformacion("Valor 1", "Valor 2");
